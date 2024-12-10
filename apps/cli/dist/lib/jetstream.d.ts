830107138b846be7b35920857d3d044f0aa612a8
sdairs <sdairs@users.noreply.github.com>
2024-12-10 16:51:59 +0000

    fix publish?

--- apps/cli/dist/lib/jetstream.d.ts
+++
@@ -1,7 +0,0 @@
-import { Destination } from '../destinations/base.js';
-export declare class JetstreamHandler {
-    private jetstream;
-    private destination;
-    constructor(destination: Destination, cursor?: number);
-    start(): Promise<void>;
-}
